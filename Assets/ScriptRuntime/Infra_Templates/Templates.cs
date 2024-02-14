eeba17a0132a21539a76c63163ed2602fb19aece
Jackutea <chenwansal1@163.com>
2024-02-14 11:57:36 +0800

    <feature> add: Load TM

--- Assets/ScriptRuntime/Infra_Templates/Templates.cs
+++ Assets/ScriptRuntime/Infra_Templates/Templates.cs
@@ -15,6 +15,15 @@ namespace WOW {
         Dictionary<string, GameObject> uis;
         AsyncOperationHandle uisOP;
 
+        Dictionary<int, RoleTM> roleTMs;
+        AsyncOperationHandle roleTMsOP;
+
+        Dictionary<int, SkillTM> skillTMs;
+        AsyncOperationHandle skillTMsOP;
+
+        Dictionary<int, BulletTM> bulletTMs;
+        AsyncOperationHandle bulletTMsOP;
+
         public Templates() {
             entities = new Dictionary<string, GameObject>();
             uis = new Dictionary<string, GameObject>();
@@ -41,11 +50,56 @@ namespace WOW {
                 }
                 uisOP = op;
             }
+            {
+                AssetLabelReference labelReference = new AssetLabelReference();
+                labelReference.labelString = "TM_Role";
+                var op = Addressables.LoadAssetsAsync<RoleTM>(labelReference, null);
+                var list = op.WaitForCompletion();
+                roleTMs = new Dictionary<int, RoleTM>();
+                foreach (var roleTM in list) {
+                    roleTMs.Add(roleTM.typeID, roleTM);
+                }
+                roleTMsOP = op;
+            }
+            {
+                AssetLabelReference labelReference = new AssetLabelReference();
+                labelReference.labelString = "TM_Skill";
+                var op = Addressables.LoadAssetsAsync<SkillTM>(labelReference, null);
+                var list = op.WaitForCompletion();
+                skillTMs = new Dictionary<int, SkillTM>();
+                foreach (var skillTM in list) {
+                    skillTMs.Add(skillTM.typeID, skillTM);
+                }
+                skillTMsOP = op;
+            }
+            {
+                AssetLabelReference labelReference = new AssetLabelReference();
+                labelReference.labelString = "TM_Bullet";
+                var op = Addressables.LoadAssetsAsync<BulletTM>(labelReference, null);
+                var list = op.WaitForCompletion();
+                bulletTMs = new Dictionary<int, BulletTM>();
+                foreach (var bulletTM in list) {
+                    bulletTMs.Add(bulletTM.typeID, bulletTM);
+                }
+                bulletTMsOP = op;
+            }
         }
 
         public void Release() {
             if (entitiesOP.IsValid()) {
                 Addressables.Release(entitiesOP);
+            }
+            if (uisOP.IsValid()) {
+                Addressables.Release(uisOP);
+            }
+            if (roleTMsOP.IsValid()) {
+                Addressables.Release(roleTMsOP);
+            }
+            if (skillTMsOP.IsValid()) {
+                Addressables.Release(skillTMsOP);
+            }
+            if (bulletTMsOP.IsValid()) {
+                Addressables.Release(bulletTMsOP);
             }
         }
 
